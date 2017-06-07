3d8c243b3f3c2107f42fb9822b3fb9d53e378a2e
Mikhailo <pantelimonovmm@gmail.com>
2017-06-07 06:25:16 +0300

    2_1 fixed @elpiankova

--- homework_2_1.py
+++ homework_2_1.py
@@ -4,20 +4,16 @@
 # Считайте четыре действительных числа от пользователя и выведите результат работы этой функции.
 
 
+# coordinates:
 x1=int(input())
 y1=int(input())
 x2=int(input())
 y2=int(input())
 
+# distance function:
 def distance (x1,y1,x2,y2):
-    if x1>=y1 :
-        print(x1-y1)
-    else:
-        print(y1-x1)
-    if x2>=y2 :
-        print(x2-y2)
-    else:
-        print(y2-x2)
 
-distance(x1,y1,x2,y2)
+    return 0.5**((x1-x2)**2+(y1-y2)**2)
+
+print(distance(x1,y1,x2,y2))
 
